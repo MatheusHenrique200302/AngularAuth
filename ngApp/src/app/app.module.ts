8f63f607e48ca1c3b31bfb0f5a380a68b7c7a97f
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2020-12-26 16:10:35 -0300

    Added HttpModule from Angular library

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { EventsComponent } from './events/events.component';
@@ -19,6 +20,7 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
