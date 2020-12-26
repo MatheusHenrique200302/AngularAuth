ecfda0376ca0b08e2eeab8528df38d63f2390440
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2020-12-26 15:29:19 -0300

    Imported Angular Forms Module

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -18,6 +18,7 @@ import { SpecialEventsComponent } from './special-events/special-events.componen
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
