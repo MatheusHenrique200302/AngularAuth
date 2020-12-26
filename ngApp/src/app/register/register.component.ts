7950884572977497701b1ef577cbd9805ccdbef8
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2020-12-26 15:34:45 -0300

    Added register Form Handlers

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerUserData = {
+    email: String,
+    password: String
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
 
+    registerUser(){
+      console.log(this.registerUserData);
+    }
 }
